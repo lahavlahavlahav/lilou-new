9bde2007afac2d7c7916a293b32778dd207faac5
lahavlahavlahav <80701234+lahavlahavlahav@users.noreply.github.com>
2026-03-05 15:52:29 +0200

    Initialize App component with routing

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+
+import { Routes, Route } from 'react-router';
+import Index from '@/pages/Index';
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  );
+}
